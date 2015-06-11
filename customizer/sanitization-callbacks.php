fe61d3c34b16a8583d1a9245a8f8043ac0d2382f
chipbennett <chip@chipbennett.net>
2015-06-10 21:15:26 -0400

    Add sanitization callbacks for HTML text, no-HTML text, and checkboxes

--- customizer/sanitization-callbacks.php
+++ customizer/sanitization-callbacks.php
@@ -11,12 +11,46 @@
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License, v2 (or newer)
  */
 
+/**
+ * Checkbox Sanitization Callback 
+ * 
+ * Sanitization callback for 'checkbox' type controls.
+ * This callback sanitizes $input as a Boolean value, either
+ * TRUE or FALSE.
+ */
+function theme_slug_sanitize_checkbox( $input ) {
+	return ( ( isset( $input ) && true == $input ) ? true : false );
+}
+
+/**
+ * HTML Text Sanitization Callback 
+ * 
+ * Sanitization callback for 'html' type text inputs. This 
+ * callback sanitizes $input for HTML allowable in posts.
+ * 
+ * @uses	wp_filter_kses()	https://developer.wordpress.org/reference/functions/wp_filter_kses	
+ */
+function theme_slug_sanitize_html( $input ) {
+	return wp_filter_kses( $input );
+}
+
+/**
+ * No HTML Text Sanitization Callback 
+ * 
+ * Sanitization callback for 'nohtml' type text inputs. This 
+ * callback sanitizes $input to remove all HTML.
+ * 
+ * @uses	wp_filter_nohtml_kses()	https://developer.wordpress.org/reference/functions/wp_filter_nohtml_kses	
+ */
+function theme_slug_sanitize_nohtml( $input ) {
+	return wp_filter_nohtml_kses( $input );
+}
 
 /**
  * Select Sanitization Callback
  * 
- * Sanitization callback for 'select' type controls. This
- * callback sanitizes $input as a slug, and then validates
+ * Sanitization callback for 'select' and 'radio' type controls. 
+ * This callback sanitizes $input as a slug, and then validates
  * $input against the choices defined for the control.
  * 
  * @uses	sanitize_key()	https://developer.wordpress.org/reference/functions/sanitize_key/
